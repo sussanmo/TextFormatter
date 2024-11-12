a40fe5ae646203f64b0540aa02f6baf2a8258bcd
Suad Mohamed <suad.mohamedh@gmail.com>
2024-11-12 10:20:10 -0600

    Add TextFormatter_CSC2040 project files and initial setup

---
+++ Main.java
@@ -0,0 +1,19 @@
+
+public class Main {
+
+    // Allow user to input document
+    // Allow user to input format type
+
+    //Create method for html formatting: < >
+
+    //Create method for markdown formatting: **, ##,
+
+    //Create method for plaint text formatting: return the file
+
+    //Create method for json tex:  { }
+
+    //Create method for xml tex:
+    public static void main(String args[]) {
+
+    }
+}
