4b78a1ed0d4634da46a7dfb4a1abfb9359a79113
Suad Mohamed <suad.mohamedh@gmail.com>
2024-11-14 10:44:40 -0600

    Add third variant of text conversion: markdownTextFormat

--- HTMLTextFormatter.java
+++ HTMLTextFormatter.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class HTMLTextFormatter {
+import java.util.ArrayList;
+
+public class HTMLTextFormatter implements IFormat{
+    @Override
+    public void format(ArrayList<String> fileContent) {
+        System.out.println("<html dir = \"ltr\" lang= \"en\">");
+        System.out.println("<head>");
+        for(int i = 0; i < fileContent.size(); i++) {
+            System.out.println("<text="+fileContent.get(i)+">");
+        }
+
+        System.out.println("</head>");
+        System.out.println("</html>");
+    }
 }
