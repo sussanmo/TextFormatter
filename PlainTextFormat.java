f6ea0f55617ffb187df77954e46e026990c76a82
Suad Mohamed <suad.mohamedh@gmail.com>
2024-11-14 10:44:00 -0600

    Add second variant of text conversion: htmlTextFormat

--- PlainTextFormat.java
+++ PlainTextFormat.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class PlainTextFormat {
+import java.util.ArrayList;
+
+public class PlainTextFormat implements IFormat {
+
+    @Override
+    public void format(ArrayList<String> fileContent) {
+        for (int i = 0; i < fileContent.size(); i++) {
+            System.out.println(fileContent.get(i));
+        }
+    }
 }
