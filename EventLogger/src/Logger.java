f98cca61b65ff59d663bc45ce097ecb2b9849ed4
Suad Mohamed <suad.mohamedh@gmail.com>
2024-11-24 23:03:55 -0600

    updated user story 3 to have history based on severity too

--- EventLogger/src/Logger.java
+++ EventLogger/src/Logger.java
@@ -1,9 +1,12 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Logger {
 
     private static Logger uniqueInstance; // single instance for singleton
     private static ArrayList<String> logHistory;
+
+    private static HashMap<String, String> severityLogHistory;
 
     private Logger(){
 
@@ -15,6 +18,7 @@ public class Logger {
             System.out.println("Creating new Logger instance");
             uniqueInstance = new Logger();
             logHistory = new ArrayList<>(); // instantiate once logger is available
+            severityLogHistory = new HashMap<>();
         }
         System.out.println("Returning instance");
         return uniqueInstance;
@@ -29,7 +33,8 @@ public class Logger {
         // make severtiy upper case to mock real log
         String logType = severity.toUpperCase();
         System.out.println(logType + ": " + log);
-        logHistory.add(log); // add to history
+        logHistory.add(logType + ": " + log); // add to history
+        severityLogHistory.put(severity.toUpperCase(),(log));
     }
 
     public void printLogHistory(){
@@ -41,6 +46,12 @@ public class Logger {
 
     public ArrayList<String> getLogHistory(){
         return logHistory;
+    }
+
+    public void printLogHistoryWithSeverity() {
+        for (String severity : severityLogHistory.keySet()) {
+            System.out.println(severity + ":" + severityLogHistory.get(severity));
+        }
     }
 
 
